2959c05525ddbac47b92afe59ae7d019b7db0379
zhangkiki <goodboyqiang@139.com>
2019-07-27 13:27:49 +0800

    note更新

---
+++ __len__.py
@@ -0,0 +1,5 @@
+import sys
+
+string = 'shiyanlou'
+i = string.__len__() #__len__返回string变量的字符数
+print(i)
---
+++ 计算1到100的和.py
@@ -0,0 +1,7 @@
+a = 0
+b = 0
+while a < 101:
+    a +=1
+    b =b+a
+    if a == 100:
+        print("The sum is {}".format(b))
