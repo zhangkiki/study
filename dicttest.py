b73c0155b1a0f00bc6138d8da78bfa86e8394024
zhangkiki <goodboyqiang@139.com>
2019-08-12 22:18:26 +0800

    noteupdate

---
+++ dicttest.py
@@ -0,0 +1,19 @@
+import sys
+
+ouptut_dict = {}
+for i, line in enumerate(sys.argv[1:]):
+    ouptut_dict[i] = line
+
+print(ouptut_dict)
+#def handle_data(arg):
+ #   for arg
+#    output_dict = arg
+#    print(output_dict)
+#def print_data(key,output_dict[key]):
+    
+#if __name__ == '__main__':
+#    for arg in sys.argv[1:]:
+#        handle_data(arg)    
+        #print(arg)
+
+#    for key in output_dict:
